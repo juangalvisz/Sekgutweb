0d79ae814e1fed68644972b6bcc18594ec1d1435
juangalvisz <136176049+juangalvisz@users.noreply.github.com>
2025-09-05 16:46:33 -0500

    filesupdate

---
+++ js/main.js
@@ -0,0 +1,105 @@
+/*===================== MOSTRAR MENÚ =======================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===================== MOSTRAR MENÚ =======================*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===================== OCULTAR MENÚ =======================*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*===================== ELIMINAR MENÚ MÓVIL =======================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+   const navMenu = document.getElementById('nav-menu');
+    //  Cuando hacemos clic en un enlace, eliminamos la clase 'show-menu' del menú
+   navMenu.classList.remove('show-menu'); 
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction));
+
+
+/*===================== CAMBIAR FONDO DEL ENCABEZADO =======================*/
+const bgHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+
+window.addEventListener('scroll', bgHeader)
+bgHeader()
+
+/*===================== SWIPER SERVICES =======================*/
+const swiperServices = new Swiper('.services__swiper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  })
+  /*===================== SHOW SCROLL UP =======================*/
+  const scrollUp = () => {
+
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        :scrollUp.classList.remove('show-scroll')
+  }
+  window.addEventListener('scroll', scrollUp)
+  scrollUp()
+
+  /*===================== SCROLL SECTIONS ACTIVE LINK =======================*/
+  const sections = document.querySelectorAll('section[id]')
+
+  const scrollActive = () => {
+    const scrollDown = window.scrollY
+    
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+              
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+  }
+  window.addEventListener('scroll', scrollActive)
+
+  
+
+/* =============== SCROLL REVEAL ANIMATION =============== */
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2500,
+    delay: 400, 
+    // reset: true, // Animation repeat
+  })
+
+  sr.reveal(`.home__content, .services__data, .services__swiper, .footer__container`)
+  sr.reveal(`.home__images`, {origin: 'bottom', delay: 1000})
+  sr.reveal(`.about__images, .contact__img`, {origin: 'left'})
+  sr.reveal(`.about__data, .contact__data`, {origin: 'right'})
+  sr.reveal(`.projects__card`, {interval: 100})
+
+    
+
